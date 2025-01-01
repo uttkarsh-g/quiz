bf3b6823cf5877912f5003b7db3574d82bc8d4e0
Alexhawak <hloji999@gmail.com>
2025-01-01 14:31:22 +0530

    its working fine

--- app.js
+++ app.js
@@ -1,3 +1,96 @@
+const questionsAndAnswers = {
+  q1: {
+    q: 'What is the full form of HTML? ',
+    o: [
+      'Hypertext markup language',
+      'Hellotrue metro language',
+      'Hypertrue metro language',
+      'Hypertext makeup lanuage',
+    ],
+    a: 'Hypertext markup language',
+  },
+  q2: {
+    q: 'Choose the correct HTML element to define important text',
+    o: ['<i>', '<important>', '<b>', '<strong>'],
+    a: '<strong>',
+  },
+  q3: {
+    q: 'What does CSS stand for?',
+    o: [
+      'Creative Style Sheets',
+      'Colorful Style Sheets',
+      'Cascading Style Sheets',
+      'Computer Style Sheets',
+    ],
+    a: 'Cascading Style Sheets',
+  },
+  q4: {
+    q: 'Which HTML tag is used to define an internal style sheet?',
+    o: ['<css>', '<script>', '<style>', 'link'],
+    a: '<style>',
+  },
+  q5: {
+    q: '',
+    o: [, , , ,],
+    a: '',
+  },
+  q6: {
+    q: '',
+    o: [, , , ,],
+    a: '',
+  },
+  q7: {
+    q: '',
+    o: [, , , ,],
+    a: '',
+  },
+  q8: {
+    q: '',
+    o: [, , , ,],
+    a: '',
+  },
+  q9: {
+    q: 'What are the different types of errors in JavaScript?',
+    o: [
+      'Load time errors',
+      'Run time errors',
+      'Logical errors',
+      'All of the above',
+    ],
+    a: 'Run time errors',
+  },
+  q10: {
+    q: 'What are the different types of Pop up boxes available in JavaScript?',
+    o: ['Alert', 'Prompt', 'Confirm', 'All of the above'],
+    a: 'All of the above',
+  },
+  q11: {
+    q: 'Which one of the following is not a keyword:',
+    o: ['if', 'with', 'debugger', 'use strict'],
+    a: 'use strict',
+  },
+  q12: {
+    q: 'Which of the following is not a framework?',
+    o: ['JavaScript .NET', 'JavaScript', 'Cocoa JS', 'jQuery'],
+    a: 'JavaScript',
+  },
+  q13: {
+    q: 'The "function" and " var" are known as',
+    o: ['Keywords', 'Prototypes', 'Declaration statements'],
+    a: 'Declaration statements',
+  },
+  q14: {
+    q: 'Which of the following scoping type does JavaScript use?',
+    o: ['Sequential', 'Segmental', 'Lexical', 'Literal'],
+    a: 'Lexical',
+  },
+  q15: {
+    q: 'Which one of the following is an ternary operator:',
+    o: ['?', ':', '-', '+'],
+    a: '?',
+  },
+};
+
 const getStarted = document.querySelector('#start');
 const userNameInput = document.querySelector('#username');
 const userName = document.querySelector('#Username');
@@ -17,9 +110,10 @@ let seconds = 30;
 let timerStop;
 let questionsCount = 1;
 sec.innerText = seconds;
+let result = 0;
+
 function countDown() {
   timerStop = setInterval(() => {
-    nextQuestion.classList.remove('see');
     seconds--;
     if (seconds < 10) {
       sec.innerText = '0' + seconds;
@@ -32,10 +126,15 @@ function countDown() {
     }
   }, 1000);
 }
+const questionList = Object.keys(questionsAndAnswers);
+let questionChange = 0;
+let qS;
 function questions() {
-  question.innerText = questionsAndAnswers.q1.q;
+  const q = questionList[questionChange];
+  qS = questionsAndAnswers[q];
+  question.innerText = qS.q;
   options.forEach((o, i) => {
-    o.innerText = questionsAndAnswers.q1.o[i];
+    o.innerText = qS.o[i];
   });
   questionNumber.innerText = questionsCount;
   questionsCount++;
@@ -44,8 +143,9 @@ function questions() {
 options.forEach((o, i) => {
   o.addEventListener('click', (element) => {
     clearInterval(timerStop);
-    if (element.target.innerText === questionsAndAnswers.q1.a) {
+    if (element.target.innerText === qS.a) {
       element.target.parentElement.classList.add('right');
+      result++;
     } else {
       element.target.parentElement.classList.add('wrong');
       close();
@@ -60,9 +160,14 @@ function close() {
 }
 nextQuestion.addEventListener('click', (e) => {
   e.stopPropagation();
+  questionChange = (questionChange + 1) % questionList.length;
   questions();
-  seconds = 31;
+  seconds = 30;
   countDown();
+  options.forEach((e) => {
+    e.parentElement.classList.remove('right', 'wrong', 'close');
+  });
+  nextQuestion.classList.remove('see');
 });
 
 getStarted.addEventListener('click', (e) => {
